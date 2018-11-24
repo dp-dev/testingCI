35099aef2c397e4c0790df7345e3642b0c1280ea
Dennis <development@studware.de>
2018-11-24 21:39:30 +0100

    Add initial commit to test travis

---
+++ TestingTravis/src/main/java/de/studware/TestingTravis/App.java
@@ -0,0 +1,17 @@
+package de.studware.TestingTravis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public int add(int no1, int no2) {
+   	 return no1 + no2;
+    }
+}
---
+++ TestingTravis/src/test/java/de/studware/TestingTravis/AppTest.java
@@ -0,0 +1,50 @@
+package de.studware.TestingTravis;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	/**
+	 * Create the test case
+	 *
+	 * @param testName
+	 *           name of the test case
+	 */
+	/*public AppTest(String testName) {
+		super(testName);
+	}
+	
+	/**
+	 * @return the suite of tests being tested
+	 
+	public static Test suite() {
+		return new TestSuite(AppTest.class);
+	}
+	
+	/**
+	 * Rigourous Test :-)
+	 
+	public void testApp() {
+		assertTrue(true);
+	}
+	*/
+	@Test
+	public void checkAddition() {
+		System.out.println("Check Test 1");
+		assertEquals(5, new App().add(2, 3));
+	}
+	
+	@Test
+	public void checkAdditionFalse() {
+		System.out.println("Check Test 2");
+		assertFalse(5 == new App().add(2, 4));
+	}
+}
