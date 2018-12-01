cdcc23b057d05fe46ce90dec5dd88602497acabd
Dennis <development@studware.de>
2018-12-01 15:11:05 +0100

    Changes to solve further problems in sonar

--- TestingTravis/src/main/java/de/studware/testingtravis/App.java
+++ TestingTravis/src/main/java/de/studware/testingtravis/App.java
@@ -1,16 +1,14 @@
 package de.studware.testingtravis;
 
 import java.util.logging.Level;
-import java.util.logging.LogRecord;
 import java.util.logging.Logger;
-import java.util.logging.LoggingPermission;
 
 /**
  * Hello world!
  *
  */
 public class App {
-	private final static Logger LOG = Logger.getLogger(App.class.getName());
+	private static final Logger LOG = Logger.getLogger(App.class.getName());
 	
 	public static void main(String[] args) {
 		LOG.log(Level.INFO, "Hello World!");
